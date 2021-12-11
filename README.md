d9f2033895486008200a2be4a1c3d46501d9922b
Maureen Rachel <maureenrachel287@gmail.com>
2021-12-11 21:31:32 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Personal Journal App
+A single user Journal Entry Web App
+
+
+
+# About
+A single user Journal Entry Web App
+
+## Features
+- Compose New Posts
+-View Existing Posts
+-Dynamic Web Pages for each Post
+
+
+## Tech
+- HTML,CSS,JS
+- Node JS, Express JS
+- MongoDB
+
+## Future Version
+-
+- [ ] Edit Posts 
+- [ ] Delete posts 
+- [ ] Add categories
+
---
+++ app.js
@@ -0,0 +1,83 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+
+const homeStartingContent = "Welcome to your personal Journal. Journalling offers a medium of comfort but more so, a medium of self-reflection.";
+const aboutContent = "A solitary reaper";
+const contactContent = "Somewhere in the universe";
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+mongoose.connect("enter mongodb atlas url here", {useNewUrlParser: true});
+
+const postSchema = {
+  title: String,
+  content: String
+};
+
+const Post = mongoose.model("Post", postSchema);
+
+app.get("/", function(req, res){
+
+  Post.find({}, function(err, posts){
+    res.render("home", {
+      startingContent: homeStartingContent,
+      posts: posts
+      });
+  });
+});
+
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
+
+app.post("/compose", function(req, res){
+  const post = new Post({
+    title: req.body.postTitle,
+    content: req.body.postBody
+  });
+
+
+  post.save(function(err){
+    if (!err){
+        res.redirect("/");
+    }
+  });
+});
+
+app.get("/posts/:postId", function(req, res){
+
+const requestedPostId = req.params.postId;
+
+  Post.findOne({_id: requestedPostId}, function(err, post){
+    res.render("post", {
+      title: post.title,
+      content: post.content
+    });
+  });
+
+});
+
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent});
+});
+
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent});
+});
+
+let port=process.env.PORT;
+if(port==null||port==''){
+  port=3000;
+}
+app.listen(port, function() {
+  console.log("Server started on port 3000");
+});
